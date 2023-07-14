80fe5a49a100b76644f42cd35bebb2c34eddc1e0
Jack Agtual <jaagtual@gmail.com>
2023-07-13 20:55:34 -0700

    Using header html for Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 function Header() {
   return (
-    <h1 className="text-center text-3xl py-5 text-slate-50 bg-slate-500">
+    <header className="text-center text-3xl py-5 text-slate-50 bg-slate-500">
       Travel Planner
-    </h1>
+    </header>
   )
 }
 

f09c3d5df98a43b8427628461e2b0160bfa0c175
Jack Agtual <jaagtual@gmail.com>
2023-10-13 14:21:24 -0700

    Cleaning up WeatherServices

--- src/services/weatherServices.ts
+++ src/services/weatherServices.ts
@@ -1,15 +1,17 @@
 import { Coordinates } from '../types/place'
 import { ForecastResponse } from '../types/weather'
 
+const BASE_URL = import.meta.env.VITE_BASE_URL
+
 export default function WeatherServices() {
   const fetchForecastData = async (
     coordinates: Coordinates,
   ): Promise<ForecastResponse> => {
-    const apiRes = await fetch(
-      `${import.meta.env.VITE_BASE_URL}/weather/forecast?lat=${coordinates?.lat}&lon=${
-        coordinates?.lon
-      }`,
-    )
+    const search = new URLSearchParams()
+    search.append('lat', coordinates?.lat.toString() || '')
+    search.append('lon', coordinates?.lon.toString() || '')
+
+    const apiRes = await fetch(`${BASE_URL}/weather/forecast?${search.toString()}`)
 
     return apiRes.json()
   }

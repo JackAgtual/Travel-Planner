b18dbde9830e46fac288687cc90109443ff33c25
Jack Agtual <jaagtual@gmail.com>
2023-10-17 21:47:09 -0700

    Adding transition to arrow scale change

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -39,13 +39,13 @@ function ImageCarousel({ images }: ImageCarouselProps) {
         className="group absolute bottom-1/2 left-0 h-full translate-y-1/2 rounded-lg px-4 transition-all hover:bg-black hover:bg-opacity-25"
         onClick={decrementImage}
       >
-        <IoIosArrowBack className="h-10 w-10 fill-white stroke-2 group-hover:scale-150" />
+        <IoIosArrowBack className="h-10 w-10 fill-white stroke-2 transition-all group-hover:scale-150" />
       </button>
       <button
         className="group absolute bottom-1/2 right-0 h-full translate-y-1/2 rounded-lg px-4 transition-all hover:bg-black hover:bg-opacity-25"
         onClick={incrementImage}
       >
-        <IoIosArrowForward className="h-10 w-10 fill-white stroke-2 group-hover:scale-150" />
+        <IoIosArrowForward className="h-10 w-10 fill-white stroke-2 transition-all group-hover:scale-150" />
       </button>
       <div className="absolute bottom-2 right-1/2 flex translate-x-1/2 gap-x-3 rounded-full bg-slate-200 bg-opacity-50 px-2 py-1">
         {images.map((_, idx) =>

31eb033de7b4c60465d29021b2572a23815660c5
Jack Agtual <jaagtual@gmail.com>
2023-10-17 21:18:16 -0700

    Increasing scale of arrow on hover

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -31,16 +31,16 @@ function ImageCarousel({ images }: ImageCarouselProps) {
         className="h-full w-full rounded-lg object-cover"
       />
       <button
-        className="absolute bottom-1/2 left-0 h-full translate-y-1/2 rounded-lg px-4 transition-all hover:bg-black hover:bg-opacity-25"
+        className="group absolute bottom-1/2 left-0 h-full translate-y-1/2 rounded-lg px-4 transition-all hover:bg-black hover:bg-opacity-25"
         onClick={decrementImage}
       >
-        <IoIosArrowBack className="h-10 w-10 fill-white stroke-2" />
+        <IoIosArrowBack className="h-10 w-10 fill-white stroke-2 group-hover:scale-150" />
       </button>
       <button
-        className="absolute bottom-1/2 right-0 h-full translate-y-1/2 rounded-lg px-4 transition-all hover:bg-black hover:bg-opacity-25"
+        className="group absolute bottom-1/2 right-0 h-full translate-y-1/2 rounded-lg px-4 transition-all hover:bg-black hover:bg-opacity-25"
         onClick={incrementImage}
       >
-        <IoIosArrowForward className="h-10 w-10 fill-white stroke-2" />
+        <IoIosArrowForward className="h-10 w-10 fill-white stroke-2 group-hover:scale-150" />
       </button>
     </div>
   )
